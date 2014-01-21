9d82eb6aa0194cc9b1f463f8fc232e357862d699
Yourtion <yourtion@gmail.com>
2014-01-21 14:32:38 +0800

    Initial Commit

---
+++ ImaginariumTests/ImaginariumTests.m
@@ -0,0 +1,34 @@
+//
+//  ImaginariumTests.m
+//  ImaginariumTests
+//
+//  Created by Yourtion on 14-1-21.
+//  Copyright (c) 2014年 Yourtion. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ImaginariumTests : XCTestCase
+
+@end
+
+@implementation ImaginariumTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
